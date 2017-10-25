433298a0aa13bbe7aabfed22c8a623ec0d38cd59
Lintang Timur <lintangtimur@protonmail.com>
2017-10-26 02:24:58 +0700

    fix dd helper function

--- src/helper/function.php
+++ src/helper/function.php
@@ -1,5 +1,12 @@
 <?php
+use Symfony\Component\VarDumper\VarDumper;
+
+/**
+ * Var dump variable
+ * @param  mixed $var variable yang akan di dump
+ * @return mixed
+ */
 function dd($var)
 {
-    dump($var);
+    VarDumper::dump($var);
 }
