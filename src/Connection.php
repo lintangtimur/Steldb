ae48edbc0747798f8fab6697aaf6e89155de25ee
Lintang Timur <lintangtimur@protonmail.com>
2017-11-02 21:00:41 +0700

    add logger option

--- src/Connection.php
+++ src/Connection.php
@@ -4,6 +4,8 @@ namespace Steldb;
 use PDO;
 use PDOException;
 use Steldb\ConnectionInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 /**
  * Conenction to database
@@ -11,17 +13,28 @@ use Steldb\ConnectionInterface;
 class Connection implements ConnectionInterface
 {
     /**
-     * [protected description]
+     * pdo instance
      * @var PDO
      */
     protected $pdo;
 
     /**
-     * Constructor
+     * Log
+     * @var Logger
      */
-    public function __construct()
+    protected $log;
+
+    /**
+     * Constructor init
+     * @param bool $config log file
+     */
+    public function __construct($config = true)
     {
         try {
+            if ($config) {
+                $this->log = new Logger("Steldb_DB");
+                $this->log->pushHandler(new StreamHandler('logs/steldb.log', Logger::DEBUG));
+            }
             $this->pdo = new PDO("{$_ENV['DB_DRIVER']}:dbname={$_ENV['DB_NAME']};host={$_ENV['DB_HOST']}", $_ENV['DB_USER'], $_ENV['DB_PASS']);
         } catch (PDOException $e) {
             die($e->getMessage());
